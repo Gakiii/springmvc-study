5e264f7786d0658fbf82ebc5add7656dde536c88
JakcyHu <hujiacheng@cug.edu.cn>
2023-02-23 17:05:21 +0800

    init

---
+++ src/main/java/com/example/springmvcstudy/SpringmvcStudyApplication.java
@@ -0,0 +1,14 @@
+package com.example.springmvcstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcStudyApplication.class, args);
+        System.out.println("start");
+    }
+
+}
