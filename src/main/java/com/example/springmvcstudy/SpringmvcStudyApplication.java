a0a6b55db7c96723bfa5ba440131cbd87020531a
JakcyHu <hujiacheng@cug.edu.cn>
2023-02-23 17:46:31 +0800

    Update SpringmvcStudyApplication.java
    test who am i

--- src/main/java/com/example/springmvcstudy/SpringmvcStudyApplication.java
+++ src/main/java/com/example/springmvcstudy/SpringmvcStudyApplication.java
@@ -9,6 +9,7 @@ public class SpringmvcStudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcStudyApplication.class, args);
         System.out.println("start");
+        // who am i
     }
 
 }
